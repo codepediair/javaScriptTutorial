6359100730dd373d74108bdaa6054436aa0bf746
Mahdi <you@example.com>
2021-10-24 11:23:54 +0330

    project_no12

---
+++ project_no12/readme.md
@@ -0,0 +1,2 @@
+# project No 12 #
+## Typing Speed Game ##
---
+++ project_no12/script.js
@@ -0,0 +1,121 @@
+const word = document.getElementById("word"),
+  text = document.getElementById("text"),
+  scoreEl = document.getElementById("score"),
+  timeEl = document.getElementById("time"),
+  endgameEl = document.getElementById("end-game-container"),
+  settingsBtn = document.getElementById("settings-btn"),
+  settings = document.getElementById("settings"),
+  settingsForm = document.getElementById("settings-form"),
+  difficultySelect = document.getElementById("difficulty");
+
+// List of words for game
+const words = [
+  "sigh",
+  "tense",
+  "airplane",
+  "ball",
+  "pies",
+  "juice",
+  "warlike",
+  "bad",
+  "north",
+  "dependent",
+  "steer",
+  "silver",
+  "highfalutin",
+  "superficial",
+  "quince",
+  "eight",
+  "feeble",
+  "admit",
+  "drag",
+  "loving",
+];
+
+let randomWord;
+
+let score = 0;
+
+let time = 10;
+
+let difficulty =
+  localStorage.getItem("difficulty") !== null
+    ? localStorage.getItem("difficulty")
+    : "medium";
+
+difficultySelect.value =
+  localStorage.getItem("difficulty") != null
+    ? localStorage.getItem("difficulty")
+    : "medium";
+
+text.focus();
+
+const timeInterval = setInterval(updateTime, 1000);
+
+function getRandomWord(){
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+function addWordToDOM(){
+    randomWord = getRandomWord();
+    word.innerHTML = randomWord;
+}
+
+function updateScore(){
+    score++;
+    scoreEl.innerHTML = score;
+}
+
+function updateTime(){
+    time--;
+    timeEl.innerHTML = time + 's';
+
+    if(time === 0) {
+        clearInterval(timeInterval);
+
+        gameOver();
+    }
+}
+
+
+function gameOver(){
+    endgameEl.innerHTML = `
+        <h1>Time ran Out</h1>
+        <p> Your Final Score is ${score}</p>
+        <button onclick="location.reload()">Reload</button>
+    `;
+
+    endgameEl.style.display = 'flex';
+}
+
+addWordToDOM();
+
+
+text.addEventListener('input', e => {
+    const insertedText = e.target.value;
+
+    if (insertedText === randomWord) {
+        addWordToDOM();
+        updateScore();
+
+        e.target.value = '';
+
+        if(difficulty === 'hard'){
+            time += 2;
+        } else if (difficulty === 'medium'){
+            time += 3;
+        } else {
+            time += 5;
+        }
+
+        updateTime();
+    }
+});
+
+settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+
+settingsForm.addEventListener('change' , e => {
+    difficulty = e.target.value;
+    localStorage.setItem('difficulty',difficulty);
+});
+
